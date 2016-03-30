33da0ddb9d495a8bb8b59c8bf63e3c7de1041d63
Rusinov.anton <rusinov.amgrade@gmail.com>
2016-03-30 11:04:10 +0300

    ref: index.js

--- app/index.js
+++ app/index.js
@@ -3,18 +3,25 @@ const yo = require('yeoman-generator'),
   mkdirp = require('mkdirp');
 
 // TODO: add yo-rc.json
-// TODO: refactor
 // TODO: add bootstrap's scss integration
 
 module.exports = yo.Base.extend({
+
   constructor: function () {
     yo.Base.apply(this, arguments);
   },
 
   initializing: function () {
     this.pkg = require('../package.json');
+
     this.options = {};
+
+    this.options.default = {
+      mixinsPath: '../../../node_modules/scss-mixins-collection/mixins/__mixins.scss'
+    };
+
     this.destinationRoot('./scss');
+
     this.config = {
       build: {
         custom: {
@@ -52,32 +59,87 @@ module.exports = yo.Base.extend({
         util: '',
         core: ''
       }
-    }
+    };
+
+    this.promptList = {
+      buildType: {
+        type: 'list',
+        name: 'build',
+        message: 'Choose scaffolds type:',
+        choices: [
+          {
+            value: 'base',
+            name: '- Base'
+          },
+          {
+            value: 'full',
+            name: '- Full'
+          },
+          {
+            value: 'custom',
+            name: '- Custom'
+          }
+        ]
+      },
+      components: {
+        type: 'checkbox',
+        name: 'custom',
+        message: 'Choose custom components to load:',
+        choices: [
+          {
+            value: 'typography',
+            checked: true,
+            name: '- typography'
+          },
+          {
+            value: 'pages',
+            name: '- pages',
+            checked: true
+          },
+          {
+            value: 'helpers',
+            name: '- helpers'
+          },
+          {
+            value: 'layout',
+            name: '- layout (extended)'
+          },
+          {
+            value: 'vars',
+            name: '- variables (extended)'
+          },
+          {
+            value: 'customMixins',
+            name: '- custom-mixins'
+          },
+          {
+            value: 'sprite',
+            name: '- sprite'
+          }
+        ]
+      }
+    };
+
+    // Wrappers for fs functions
+    this.fsCopy = function (from, to) {
+      this.fs.copy(
+        path.join(this.templatePath(), from),
+        path.join(this.destinationPath(), to)
+      );
+    }.bind(this);
+    this.fsWrite = function (to, string) {
+      this.fs.write(path.join(this.destinationPath(), to), string);
+    }.bind(this);
+    this.fsRead = function (from) {
+      return this.fs.read(path.join(this.templatePath(), from));
+    }.bind(this);
   },
 
   prompting: {
     build: function () {
       var done = this.async();
-      this.prompt(
-        {
-          type: 'list',
-          name: 'build',
-          message: 'Choose scaffolds type:',
-          choices: [
-            {
-              value: 'base',
-              name: '- Base'
-            },
-            {
-              value: 'full',
-              name: '- Full'
-            },
-            {
-              value: 'custom',
-              name: '- Custom'
-            }
-          ]
-        },
+
+      this.prompt(this.promptList.buildType,
         function (response) {
           this.options.build = this.config.build[response.build];
           if (response.build === 'custom') this.options.custom = true;
@@ -87,45 +149,9 @@ module.exports = yo.Base.extend({
     },
     custom: function () {
       var done = this.async();
+
       if (this.options.custom) {
-        this.prompt(
-          {
-            type: 'checkbox',
-            name: 'custom',
-            message: 'Choose custom components to load:',
-            choices: [
-              {
-                value: 'typography',
-                checked: true,
-                name: '- typography'
-              },
-              {
-                value: 'pages',
-                name: '- pages',
-                checked: true
-              },
-              {
-                value: 'helpers',
-                name: '- helpers'
-              },
-              {
-                value: 'layout',
-                name: '- layout (extended)'
-              },
-              {
-                value: 'vars',
-                name: '- variables (extended)'
-              },
-              {
-                value: 'customMixins',
-                name: '- custom-mixins'
-              },
-              {
-                value: 'sprite',
-                name: '- sprite'
-              }
-            ]
-          },
+        this.prompt(this.promptList.components,
           function (response) {
             response.custom.forEach(function (opt) {
               this.options.build[opt] = true;
@@ -155,11 +181,11 @@ module.exports = yo.Base.extend({
             {
               type: 'input',
               name: 'mixinsPath',
-              message: 'Relative path to the _mixins.scss file ( default: ../../../node_modules/scss-mixins-collection/mixins/_mixins.scss ):'
+              message: 'Relative path to the _mixins.scss file from this folder ( default: ' + this.options.default.mixinsPath.substr(6) + ' ):'
             },
             function (response) {
-              var done = this.async();
-              this.options.mixinsPath = response.mixinsPath || '../../../node_modules/scss-mixins-collection/mixins/_mixins.scss';
+              this.options.mixinsPath = response.mixinsPath ? '../../' + response.mixinsPath : false || this.options.default.mixinsPath;
+              console.log(this.options.mixinsPath);
               done();
             }.bind(this)
           );
@@ -172,31 +198,33 @@ module.exports = yo.Base.extend({
   writing: {
     vars: function () {
       var done = this.async();
-      var from = this.options.build.vars ? 'extended' : 'base';
-      mkdirp(path.join(this.destinationPath(), 'util/'));
-      this.fs.copy(
-        path.join(this.templatePath(), from, 'variables'),
-        path.join(this.destinationPath(), 'util/variables')
-      );
+      var from = this.options.build.vars ? 'extended/variables' : 'base/variables';
+
+      mkdirp.sync(path.join(this.destinationPath(), 'util/'));
+
+      this.fsCopy(from, path.join(this.destinationPath(), 'util/variables'));
+
       done();
     },
     mixins: function () {
       var done = this.async();
-      this.config.files.util += '\n' + this.fs.read(path.join(this.templatePath(), 'import/util/variables'));
+
+      this.config.files.util += '\n' + this.fsRead('import/util/variables');
+
       if (this.options.build.mixins) {
         this.config.files.util += '\n' + '// Mixins library\n@import "' + this.options.mixinsPath + '";' + '\n';
       }
+
       done();
     },
     customMixins: function () {
       var done = this.async();
       if (this.options.build.customMixins) {
-        this.config.files.util += '\n' + this.fs.read(path.join(this.templatePath(), 'import/util/custom-mixins'));
 
-        this.fs.copy(
-          path.join(this.templatePath(), 'extended', 'custom-mixins'),
-          path.join(this.destinationPath(), 'util/custom-mixins')
-        );
+        this.config.files.util += '\n' + this.fsRead('import/util/custom-mixins');
+
+        this.fsCopy('extended/custom-mixins', 'util/custom-mixins');
+
       }
       done();
     },
@@ -204,79 +232,66 @@ module.exports = yo.Base.extend({
       var done = this.async();
 
       if (this.options.build.sprite) {
-        this.config.files.util += '\n' + this.fs.read(path.join(this.templatePath(), 'import/util/spriting'));
 
-        this.fs.copy(
-          path.join(this.templatePath(), 'extended', '_spriting.scss'),
-          path.join(this.destinationPath(), 'util/_spriting.scss')
-        );
+        this.config.files.util += '\n' + this.fsRead('import/util/spriting');
+        this.fsCopy('extended', '_spriting.scss', 'util/_spriting.scss');
+
       }
+
       done();
     },
     util: function () {
       var done = this.async();
-      this.fs.write(path.join(this.destinationPath(), 'util/__util.scss'), this.config.files.util);
-      this.config.files.core += '\n' + this.fs.read(path.join(this.templatePath(),'import/core/util'));
+      this.fsWrite('util/__util.scss', this.config.files.util);
+      this.config.files.core += '\n' + this.fsRead('import/core/util');
       done();
     },
     typography: function () {
       var done = this.async();
       if (this.options.build.typography) {
-        this.config.files.util += '\n' + this.fs.read(path.join(this.templatePath(),'import/core/typography'));
-        this.fs.copy(
-          path.join(this.templatePath(), 'base', 'typography'),
-          path.join(this.destinationPath(), 'typography')
-        );
+        this.config.files.util += '\n' + this.fsRead('import/core/typography');
+        this.fsCopy('base/typography', 'typography');
       }
       done();
     },
     pages: function () {
       var done = this.async();
       if (this.options.build.pages) {
-        this.config.files.core += '\n' + this.fs.read(path.join(this.templatePath(),'import/core/pages'));
-        mkdirp(path.join(this.destinationPath(), 'pages'));
-        this.fs.write(path.join(this.destinationPath(), 'pages/__pages.scss'), '// Sets import for pages scss files');
+        this.config.files.core += '\n' + this.fsRead('import/core/pages');
+        mkdirp.sync(path.join(this.destinationPath(), 'pages'));
+        this.fsWrite('pages/__pages.scss', '// Sets import for pages scss files');
       }
       done();
     },
     layout: function () {
       var done = this.async(),
-        from = this.options.build.layout ? 'extended' : 'base';
+        from = this.options.build.layout ? 'extended/layout' : 'base/layout';
 
-      this.config.files.core += '\n' + this.fs.read(path.join(this.templatePath(),'import/core/layout'));
-      this.fs.copy(
-        path.join(this.templatePath(), from, 'layout'),
-        path.join(this.destinationPath(), 'layout')
-      );
+      this.config.files.core += '\n' + this.fsRead('import/core/layout');
+      this.fsCopy(from, 'layout');
 
       done();
     },
     helpers: function () {
       var done = this.async();
       if (this.options.build.helpers) {
-        this.config.files.core += '\n' + this.fs.read(path.join(this.templatePath(),'import/core/helpers'));
-        this.fs.copy(
-          path.join(this.templatePath(), 'extended/helpers'),
-          path.join(this.destinationPath(), 'helpers')
-        );
+        this.config.files.core += '\n' + this.fsRead('import/core/helpers');
+        this.fsCopy('extended/helpers', 'helpers');
       }
       done();
     },
-    core: function() {
+    core: function () {
       var done = this.async();
-      this.fs.write(path.join(this.destinationPath(), 'core.scss'), this.config.files.core);
+      this.fsWrite('core.scss', this.config.files.core);
       done();
     },
-    config: function(){
-      this.fs.copy(
-        path.join(this.templatePath(), 'config.rb'),
-        path.join(this.destinationPath(), 'config.rb')
-      );
+    config: function () {
+      this.fsCopy('config.rb', 'config.rb');
     }
   },
 
   end: function () {
-    console.log('bye');
+    console.log('*** Happy styling indeed! ***');
   }
 
 });
