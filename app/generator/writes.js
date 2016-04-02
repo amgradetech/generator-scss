ffa04af3b7d295183f6b4cd7c66d3e79baca8572
Rusinov.Anton <rusinov.amgrade@gmail.com>
2016-04-02 23:00:30 +0300

    fix for bootstrap grid build

--- app/generator/writes.js
+++ app/generator/writes.js
@@ -5,15 +5,17 @@ module.exports = function(_this) {
         this.fsWrite('bootstrap.scss', this.fsTpl('bootstrap/bootstrap', {bootstrapScssPath: _this.paths.bootstrapScssPath}));
       }.bind(_this),
       fullFlex: function () {
-        var content = this.fsRead('bootstrap/bootstrap-flex') + '\n';
+        var content = this.fsTpl('bootstrap/bootstrap-flex', {enableFlex: 'true'}) + '\n';
         content += this.fsTpl('bootstrap/bootstrap', {bootstrapScssPath: _this.paths.bootstrapScssPath});
         this.fsWrite('bootstrap.scss', content);
       }.bind(_this),
       grid: function () {
-        this.fsWrite('bootstrap.scss', this.fsTpl('bootstrap/bootstrap-grid', {bootstrapScssPath: _this.paths.bootstrapScssPath}));
+        var content = this.fsTpl('bootstrap/bootstrap-flex', {enableFlex: 'false'}) + '\n';
+        content += this.fsTpl('bootstrap/bootstrap-grid', {bootstrapScssPath: _this.paths.bootstrapScssPath};
+        this.fsWrite('bootstrap.scss', content);
       }.bind(_this),
       gridFlex: function () {
-        var content = this.fsRead('bootstrap/bootstrap-flex') + '\n';
+        var content = this.fsTpl('bootstrap/bootstrap-flex', {enableFlex: 'true'}) + '\n';
         content += this.fsTpl('bootstrap/bootstrap-grid', {bootstrapScssPath: _this.paths.bootstrapScssPath});
         this.fsWrite('bootstrap.scss', content);
       }.bind(_this),
